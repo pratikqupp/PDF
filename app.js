f8d9c981b7cffa5627fac49b855a7869ee775ab0
Pratik Pansare <113935701+pratikqupp@users.noreply.github.com>
2024-05-24 10:39:34 +0530

    kk

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/', async (req, res) => {
     const settings = requestData.settings ? requestData.settings : {};
     const prescriptionPrintSettings = requestData.prescriptionPrintSettings ? requestData.prescriptionPrintSettings : {};
     const medicalInfoOfGynecModule = requestData.medicalInfoOfGynecModule ? requestData.medicalInfoOfGynecModule : {};
-    const prescriptionFontConfiguration = requestData.prescriptionFontConfiguration ? requestData.prescriptionFontConfiguration : {};
+    const prescriptionFontConfiguration = requestData.prescriptionPrintSettings.prescriptionFontConfiguration ? requestData.prescriptionPrintSettings.prescriptionFontConfiguration : {};
     const language = getLanguage(requestData.language ? requestData.language.trim() : "");
 
     function getLanguage(language) {
@@ -67,45 +67,45 @@ app.post('/', async (req, res) => {
     <div>
         <div id="invoice-mid" style="display: flex; justify-content: space-between;">
             ${entityLogo ? `<img src="data:image/png;base64,${entityLogo}" width="68" height="68" style="display: block; margin: 0 auto;"><br>` : ``}
-            <div class="info" style="width: calc(50% - 25px);">
-                <h2>${clinicName}</h2>
-                <p style="max-width: 100%;">${clinicAddress}<br>
-    <span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Contact :</strong> ${clinicContact}</span>
+            <div class="info" style="width: calc(50% - 10px);">
+                <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">${clinicName}</h2>
+                <p style="max-width: 100%; font-size: ${prescriptionFontConfiguration.fontHeader}px;">${clinicAddress}<br>
+    <span style="font-size: ${prescriptionFontConfiguration.fontHeader}px;"><strong>Contact :</strong> ${clinicContact}</span>
   </span></p>
             </div>
-            <div id="project" style="width: calc(50% - 25px);">
-                <h2>${doctorName}</h2>
-                <p class="degree" style="max-width: 100%; word-wrap: break-word;">${doctorDegress}</span></p>
+            <div id="project" style="width: calc(50% - 10px);">
+                <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">${doctorName}</h2>
+                <p class="degree" style="max-width: 100%; word-wrap: break-word; font-size: ${prescriptionFontConfiguration.fontHeader}px;">${doctorDegress}</span></p>
             </div>
         </div>
         <hr style="background-color: rgb(226, 223, 223);">
     </div>
     ` : '';
-    const symptomsLayout = settings.symptoms ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Symptoms :</strong> ${symptoms}</span></span></p>
+    const symptomsLayout = settings.symptoms ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Symptoms :</strong></span> <span style="font-size: ${prescriptionFontConfiguration.fontDetails}px;">${symptoms}</span></p>
 <br>`: '';
-    const clinicalFindingLayout = settings.clinicalFinding ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Clinical Findings :</strong> ${clinicalFindings}</span></span></p>
+    const clinicalFindingLayout = settings.clinicalFinding ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Clinical Findings :</strong></span> <span style="font-size: ${prescriptionFontConfiguration.fontDetails}px;"> ${clinicalFindings}</span></p>
 <br>`: '';
 
     const diagnosesLayout = settings.diagnosis ?
-        `${diagnoses ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Diagnosis :</strong> ${diagnoses}</span></p>
+        `${diagnoses ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Diagnosis :</strong> </span> <span style="font-size: ${prescriptionFontConfiguration.fontDetails}px;">${diagnoses}</span></p>
 <br>`: ``}
 `: ``;
     const medicineHeaders = ` <tr class="tabletitle">
     <td class="sr">
-        <h2>RX </h2>
+        <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">RX </h2>
     </td>
     <td class="item">
-        <h2>BRANDS </h2>
+    <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">BRANDS </h2>
     </td>
     <td class="Hours">
-        <h2>FREQUENCY </h2>
+    <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">FREQUENCY </h2>
     </td>
     <td class="Rate">
-        <h2>DURATION </h2>
+    <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">DURATION </h2>
     </td>
     ${settings.totalQuantity ? `
     <td class="subtotal">
-        <h2>QUANTITY </h2>
+    <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;">QUANTITY </h2>
     </td>
     `: ``}
 </tr>`;
@@ -117,21 +117,21 @@ app.post('/', async (req, res) => {
             ${medicineDTOS.map((medicine, index) => `
                 <tr class="service">
                     <td class="tableitem">
-                        <p class="itemtext">${index + 1}</p>
+                        <p style="font-size: ${prescriptionFontConfiguration.fontDetails}px;" class="itemtext">${index + 1}</p>
                     </td>
                     <td class="tableitem">
-                        <p class="itemtext">${medicine.brandName ? medicine.brandName : ""}</p>
-                        ${settings.medicineComposition ? `<p class="itemtext">${medicine.genericName ? medicine.genericName : ""}</p>` : ``}
+                        <p style="font-size: ${prescriptionFontConfiguration.fontDetails}px;" class="itemtext">${medicine.brandName ? medicine.brandName : ""}</p>
+                        ${settings.medicineComposition ? `<p style="font-size: ${prescriptionFontConfiguration.fontGeneric}px;" class="itemtext">${medicine.genericName ? medicine.genericName : ""}</p>` : ``}
                     </td>
                     <td class="tableitem">
-                        <p class="itemtext">${medicine.frequancy ? medicine.frequancy : ""}</p>
+                        <p style="font-size: ${prescriptionFontConfiguration.fontDetails}px;" class="itemtext">${medicine.frequancy ? medicine.frequancy : ""}</p>
                     </td>
                     <td class="tableitem">
-                        <p class="itemtext">${medicine.duration ? medicine.duration : ""}</p>
+                        <p style="font-size: ${prescriptionFontConfiguration.fontDetails}px;" class="itemtext">${medicine.duration ? medicine.duration : ""}</p>
                     </td>
                     ${settings.totalQuantity ? `
                         <td class="tableitem">
-                            <p class="itemtext">${medicine.quantity ? medicine.quantity : ""}</p>
+                            <p style="font-size: ${prescriptionFontConfiguration.fontDetails}px;" class="itemtext">${medicine.quantity ? medicine.quantity : ""}</p>
                         </td>
                     ` : ``}      
                 </tr>`
@@ -143,17 +143,17 @@ app.post('/', async (req, res) => {
 ` : '';
 
     const investigationsLayout =
-        `${investigations ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Investigations :</strong> ${investigations}</span></p>
+        `${investigations ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Investigations :</strong></span> <span style="font-size: ${prescriptionFontConfiguration.fontDetails}px;"> ${investigations}</span></p>
     <br>`: ``}
 `;
     const instructionsLayout = settings.instruction ? `
-    ${instructions ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>General instructions :</strong></span></p>
-<p>${instructions}</p>` : ``}
+    ${instructions ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>General instructions :</strong></span></p>
+<p style="font-size: ${prescriptionFontConfiguration.fontDetails}px;" >${instructions}</p>` : ``}
 <br>
 `: '';
     const footer = settings.signature ? ` <div>
-        <h2>${doctorName}</h2>
-        <p class="degree" style="max-width: 600px; word-wrap: break-word;">${doctorDegress}</p>
+        <h2 style="font-size: ${prescriptionFontConfiguration.fontHeader}px;" >${doctorName}</h2>
+        <p class="degree" style="max-width: 600px; word-wrap: break-word; font-size: ${prescriptionFontConfiguration.fontHeader}px;">${doctorDegress}</p>
     `: ``;
     const htmlContent =
         `
@@ -164,12 +164,6 @@ app.post('/', async (req, res) => {
     <title>Q UP Bill</title>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+${language}:wght@400&display=swap" rel="stylesheet">
     <style>
-
-        * {
-            margin: ${prescriptionPrintSettings.marginTop}   ${prescriptionPrintSettings.marginRight} ${prescriptionPrintSettings.marginBottom} ${prescriptionPrintSettings.marginLeft};
-            box-sizing: border-box;
-        }
-
         body {
             font-family: 'Noto Sans ${language}', sans-serif;
             background-repeat: repeat-y;
@@ -187,22 +181,22 @@ app.post('/', async (req, res) => {
         }
 
         h1 {
-            font-size: 1.5em;
+            // font-size: 1.5em;
             color: #222;
         }
 
         h2 {
-            font-size: .9em;
+            // font-size: .9em;
         }
 
         h3 {
-            font-size: 1.2em;
+            // font-size: 1.2em;
             font-weight: 300;
             line-height: 2em;
         }
 
         p {
-            font-size: .7em;
+            // font-size: .7em;
             color: #666;
             line-height: 1.2em;
         }
@@ -210,7 +204,6 @@ app.post('/', async (req, res) => {
         #invoiceholder {
             width: 100%;
             height: 100%;
-            padding-top: 50px;
         }
 
         #headerimage {
@@ -229,12 +222,12 @@ app.post('/', async (req, res) => {
         }
 
         [id*='invoice-'] {
-            padding: 30px;
+            // padding: 30px;
         }
 
         [id*='bottomline-'] {
             border-bottom: 1px solid #EEE;
-            padding: 30px;
+            // padding: 30px;
         }
 
         #invoice-bot {
@@ -280,12 +273,12 @@ app.post('/', async (req, res) => {
         }
 
         td {
-            padding: 5px 0 5px 15px;
+            padding: 5px 0 5px 5px;
             border: 1px solid #EEE
         }
 
         .tabletitle {
-            padding: 5px;
+            padding: 0px;
             background: #EEE;
         }
 
@@ -298,7 +291,7 @@ app.post('/', async (req, res) => {
         }
 
         .itemtext {
-            font-size: 11px;
+            // font-size: 11px;
         }
 
         #legalcopy {
@@ -354,25 +347,37 @@ app.post('/', async (req, res) => {
 </head>
 
 <body>
-    <div id="invoiceholder">
-        ${entityLayout}
-        <div>
-        <div id="invoice-mid" style="display: flex; justify-content: space-between;">
-            <div class="info" style="width: calc(50% - 25px);">
-            <h2>Name : ${patientName}</h2>
-            <p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Age/Gender :</strong> ${patientAgeGender}</span></p>
-            </div>
-            <div id="project" style="width: calc(50% - 25px);">
-            <h2><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Date :</strong> ${prescriptionDate}</span></h2>
-            <p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Mobile No :</strong> ${patientMobileNumber}</span></p>
+<div  style=" margin-top: ${prescriptionPrintSettings.marginTop * 37.7952755906}px; 
+margin-bottom: ${prescriptionPrintSettings.marginBottom * 37.7952755906}px; 
+margin-left: ${prescriptionPrintSettings.marginLeft * 37.7952755906}px; 
+margin-right: ${prescriptionPrintSettings.marginRight * 37.7952755906}px;">
+<div >
+${entityLayout}
+<div> 
+</div>
+<div>
+    <div id="invoice-mid" style="display: flex; justify-content: space-between;">
+        <div class="info" style="width: calc(50% - 25px);">
+            <h2 style="max-width: 100%; font-size: ${prescriptionFontConfiguration.fontTitle}px;">${patientName}</h2>
+            <p style="max-width: 100%; font-size: ${prescriptionFontConfiguration.fontTitle}px;">
+                <strong>Age/Gender :</strong> ${patientAgeGender}
+            </p>
         </div>
+        <div id="project" style="width: calc(50% - 25px);">
+            <h2 style="max-width: 100%; font-size: ${prescriptionFontConfiguration.fontTitle}px;">Date : ${prescriptionDate}</h2>
+            <p  style="max-width: 100%; word-wrap: break-word; font-size: ${prescriptionFontConfiguration.fontTitle}px;">
+                <strong>Mobile No :</strong> ${patientMobileNumber}
+            </p>
         </div>
     </div>
+</div>
+</div>
+
         <hr style="background-color: rgb(226, 223, 223) margin: 0px ${prescriptionPrintSettings.marginRight} 0px ${prescriptionPrintSettings.marginLeft};">
         <div id="invoice" class="effect2">
             <div id="invoice-bot">
                 <div id="left-section">
-                ${vital ? settings.vitals ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Vital : </strong> ${vital}</span></p> <br>` : `` : ''}
+                ${vital ? settings.vitals ? `<p><span style="font-size: ${prescriptionFontConfiguration.fontTitle}px;"><strong>Vital : </strong> ${vital}</span></p> <br>` : `` : ''}
                     ${symptoms ? symptomsLayout : ''}
                     ${clinicalFindings ? clinicalFindingLayout : ''}
                     ${diagnoses ? diagnosesLayout : ''}
@@ -412,6 +417,7 @@ app.post('/', async (req, res) => {
 </div>
 </div>
 </div>
+    </div>
     </div>
     <script>
     </script>
